1d8e513644c5eaf45f622bf17ab790e93cc0d65e
riclin1 <riclin1@gmail.com>
2015-07-06 23:57:00 +0800

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("JUR");
+    case mBTC: return QString("mJUR");
+    case uBTC: return QString::fromUtf8("μJUR");
     default: return QString("???");
     }
 }
